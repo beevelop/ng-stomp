51a6b960c083158cf18f14e886917aaf3d0edd20
Maik Hummel <yo@beevelop.com>
2016-01-10 09:36:03 +0100

    Add task for standalone file (includes dependencies)

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,8 +2,17 @@ module.exports = function (grunt) {
   grunt.initConfig({
     uglify: {
       main: {
+        options: {
+          preserveComments: 'some'
+        },
         files: {
-          'ng-stomp.min.js': ['ng-stomp.js']
+          'ng-stomp.min.js': ['ng-stomp.js'],
+          'ng-stomp.standalone.min.js': [
+            'bower_components/sockjs/sockjs.min.js',
+            'bower_components/stomp-websocket/lib/stomp.min.js',
+            'bower_components/stompie/stompie-min.js',
+            'ng-stomp.js'
+          ]
         }
       }
     },
