2974f0c8307ebed501c514efd0e95b4e8b98708a
Maik Hummel <yo@beevelop.com>
2016-01-10 08:56:46 +0100

    Update Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,18 @@
 module.exports = function (grunt) {
-	grunt.initConfig({
-		uglify: {
-			main: {
-				files: {
-					'ng-stomp.min.js': ['ng-stomp.js']
-				}
-			}
-		},
-        jshint: {
-            all: ['*.js', '!*.min.js']
+  grunt.initConfig({
+    uglify: {
+      main: {
+        files: {
+          'ng-stomp.min.js': ['ng-stomp.js']
         }
- 	});
+      }
+    },
+    jshint: {
+      all: ['*.js', '!*.min.js']
+    }
+  });
 
- 	grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.registerTask('default', ['jshint', 'uglify']);
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.registerTask('default', ['jshint', 'uglify']);
 };
