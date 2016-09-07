1fb50f5033cbb40fbc990dd81b63de5a3f29509b
Corey Wilson <corey@commercesignals.com>
2016-09-07 10:36:18 -0400

    add umd support

--- src/ng-stomp.js
+++ src/ng-stomp.js
@@ -5,76 +5,92 @@
  * @author Maik Hummel <m@ikhummel.com>
  * @license MIT
  */
+(function (root, factory) {
+  'use strict';
 
-/*global
-    angular, SockJS, Stomp */
+  if (false && typeof define === 'function' && define.amd) {
+    define(['angular', 'sockjs-client', 'stompjs'], factory);
 
-angular
-  .module('ngStomp', [])
-  .service('$stomp', [
-    '$rootScope', '$q',
-    function ($rootScope, $q) {
-      this.sock = null
-      this.stomp = null
-      this.debug = null
+  } else if (typeof module !== 'undefined' && typeof module.exports === 'object') {
+    module.exports = factory(require('angular'), require('sockjs-client'), require('stompjs'));
 
-      this.setDebug = function (callback) {
-        this.debug = callback
-      }
+  } else {
+    return factory(root.angular, root.SockJS, root.Stomp);
+  }
 
-      this.connect = function (endpoint, headers, errorCallback) {
-        headers = headers || {}
+}(window, function (angular, SockJS, Stomp) {
+  'use strict';
 
-        var dfd = $q.defer()
+  var moduleName = 'ngStomp';
 
-        this.sock = new SockJS(endpoint)
-        this.stomp = Stomp.over(this.sock)
-        this.stomp.debug = this.debug
-        this.stomp.connect(headers, function (frame) {
-          dfd.resolve(frame)
-        }, function (err) {
-          dfd.reject(err)
-          errorCallback(err)
-        })
+  angular
+    .module(moduleName, [])
+    .service('$stomp', [
+      '$rootScope', '$q',
+      function ($rootScope, $q) {
+        this.sock = null
+        this.stomp = null
+        this.debug = null
 
-        return dfd.promise
-      }
-
-      this.disconnect = function () {
-        var dfd = $q.defer()
-        this.stomp.disconnect(dfd.resolve)
-        return dfd.promise
-      }
-
-      this.subscribe = this.on = function (destination, callback, headers) {
-        headers = headers || {}
-        return this.stomp.subscribe(destination, function (res) {
-          var payload = null
-          try {
-            payload = JSON.parse(res.body)
-          } finally {
-            if (callback) {
-              callback(payload, res.headers, res)
-            }
-          }
-        }, headers)
-      }
-
-      this.unsubscribe = this.off = function (subscription) {
-        subscription.unsubscribe()
-      }
-
-      this.send = function (destination, body, headers) {
-        var dfd = $q.defer()
-        try {
-          var payloadJson = JSON.stringify(body)
-          headers = headers || {}
-          this.stomp.send(destination, headers, payloadJson)
-          dfd.resolve()
-        } catch (e) {
-          dfd.reject(e)
+        this.setDebug = function (callback) {
+          this.debug = callback
         }
-        return dfd.promise
-      }
-    }]
-)
+
+        this.connect = function (endpoint, headers, errorCallback) {
+          headers = headers || {}
+          var dfd = $q.defer()
+
+          this.sock = new SockJS(endpoint)
+          this.stomp = Stomp.over(this.sock)
+          this.stomp.debug = this.debug
+          this.stomp.connect(headers, function (frame) {
+            dfd.resolve(frame)
+          }, function (err) {
+            dfd.reject(err)
+            errorCallback(err)
+          })
+
+          return dfd.promise
+        }
+
+        this.disconnect = function () {
+          var dfd = $q.defer()
+          this.stomp.disconnect(dfd.resolve)
+          return dfd.promise
+        }
+
+        this.subscribe = this.on = function (destination, callback, headers) {
+          headers = headers || {}
+          return this.stomp.subscribe(destination, function (res) {
+            var payload = null
+            try {
+              payload = JSON.parse(res.body)
+            } finally {
+              if (callback) {
+                callback(payload, res.headers, res)
+              }
+            }
+          }, headers)
+        }
+
+        this.unsubscribe = this.off = function (subscription) {
+          subscription.unsubscribe()
+        }
+
+        this.send = function (destination, body, headers) {
+          var dfd = $q.defer()
+          try {
+            var payloadJson = JSON.stringify(body)
+            headers = headers || {}
+            this.stomp.send(destination, headers, payloadJson)
+            dfd.resolve()
+          } catch (e) {
+            dfd.reject(e)
+          }
+          return dfd.promise
+        }
+      }]
+  )
+
+  return moduleName;
+}));
