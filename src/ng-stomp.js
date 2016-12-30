9402bbe1daab45d1cc128c0c6c747947771a843c
Maik Hummel <yo@beevelop.com>
2016-12-30 16:04:49 +0100

    Encapsulate in anonymous function to fix concat bugs (close #13, close #10)

--- src/ng-stomp.js
+++ src/ng-stomp.js
@@ -8,82 +8,83 @@
 
 /*global
     angular, SockJS, Stomp */
+(function () {
+  angular
+    .module('ngStomp', [])
+    .service('$stomp', [
+      '$rootScope', '$q',
+      function ($rootScope, $q) {
+        this.sock = null
+        this.stomp = null
+        this.debug = null
 
-angular
-  .module('ngStomp', [])
-  .service('$stomp', [
-    '$rootScope', '$q',
-    function ($rootScope, $q) {
-      this.sock = null
-      this.stomp = null
-      this.debug = null
-
-      this.setDebug = function (callback) {
-        this.debug = callback
-      }
-
-      this.connect = function (endpoint, headers, errorCallback, sockjsOpts) {
-        headers = headers || {}
-        sockjsOpts = sockjsOpts || {}
-
-        var dfd = $q.defer()
-
-        this.sock = new SockJS(endpoint, null, sockjsOpts)
-        this.sock.onclose = function () {
-          if (angular.isFunction(errorCallback)) {
-            errorCallback(new Error('Connection broken'))
-          }
+        this.setDebug = function (callback) {
+          this.debug = callback
         }
 
-        this.stomp = Stomp.over(this.sock)
-        this.stomp.debug = this.debug
-        this.stomp.connect(headers, function (frame) {
-          dfd.resolve(frame)
-        }, function (err) {
-          dfd.reject(err)
-          if (angular.isFunction(errorCallback)) {
-            errorCallback(err)
-          }
-        })
+        this.connect = function (endpoint, headers, errorCallback, sockjsOpts) {
+          headers = headers || {}
+          sockjsOpts = sockjsOpts || {}
 
-        return dfd.promise
-      }
+          var dfd = $q.defer()
 
-      this.disconnect = function () {
-        var dfd = $q.defer()
-        this.stomp.disconnect(dfd.resolve)
-        return dfd.promise
-      }
-
-      this.subscribe = this.on = function (destination, callback, headers) {
-        headers = headers || {}
-        return this.stomp.subscribe(destination, function (res) {
-          var payload = null
-          try {
-            payload = JSON.parse(res.body)
-          } finally {
-            if (callback) {
-              callback(payload, res.headers, res)
+          this.sock = new SockJS(endpoint, null, sockjsOpts)
+          this.sock.onclose = function () {
+            if (angular.isFunction(errorCallback)) {
+              errorCallback(new Error('Connection broken'))
             }
           }
-        }, headers)
-      }
 
-      this.unsubscribe = this.off = function (subscription) {
-        subscription.unsubscribe()
-      }
+          this.stomp = Stomp.over(this.sock)
+          this.stomp.debug = this.debug
+          this.stomp.connect(headers, function (frame) {
+            dfd.resolve(frame)
+          }, function (err) {
+            dfd.reject(err)
+            if (angular.isFunction(errorCallback)) {
+              errorCallback(err)
+            }
+          })
 
-      this.send = function (destination, body, headers) {
-        var dfd = $q.defer()
-        try {
-          var payloadJson = JSON.stringify(body)
-          headers = headers || {}
-          this.stomp.send(destination, headers, payloadJson)
-          dfd.resolve()
-        } catch (e) {
-          dfd.reject(e)
+          return dfd.promise
         }
-        return dfd.promise
-      }
-    }]
-)
+
+        this.disconnect = function () {
+          var dfd = $q.defer()
+          this.stomp.disconnect(dfd.resolve)
+          return dfd.promise
+        }
+
+        this.subscribe = this.on = function (destination, callback, headers) {
+          headers = headers || {}
+          return this.stomp.subscribe(destination, function (res) {
+            var payload = null
+            try {
+              payload = JSON.parse(res.body)
+            } finally {
+              if (callback) {
+                callback(payload, res.headers, res)
+              }
+            }
+          }, headers)
+        }
+
+        this.unsubscribe = this.off = function (subscription) {
+          subscription.unsubscribe()
+        }
+
+        this.send = function (destination, body, headers) {
+          var dfd = $q.defer()
+          try {
+            var payloadJson = JSON.stringify(body)
+            headers = headers || {}
+            this.stomp.send(destination, headers, payloadJson)
+            dfd.resolve()
+          } catch (e) {
+            dfd.reject(e)
+          }
+          return dfd.promise
+        }
+      }]
+  )
+})()
